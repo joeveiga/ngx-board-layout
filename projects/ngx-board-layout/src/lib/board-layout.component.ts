c6a65100a335c1582a714f53be64ab60a4808a6e
Jose Veiga <jose86veiga@gmail.com>
2021-01-30 22:41:18 -0500

    add resize polyfill

--- projects/ngx-board-layout/src/lib/board-layout.component.ts
+++ projects/ngx-board-layout/src/lib/board-layout.component.ts
@@ -4,12 +4,17 @@ import {
   ContentChildren,
   QueryList,
   OnDestroy,
-  HostBinding,
   AfterContentInit,
-  Input
+  Input,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  RendererStyleFlags2
 } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import ResizeObserver from 'resize-observer-polyfill';
+
 import { BoardCardDirective } from './board-card.directive';
 
 @Component({
@@ -18,20 +23,23 @@ import { BoardCardDirective } from './board-card.directive';
   styleUrls: ['./board-layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BoardLayoutComponent implements AfterContentInit, OnDestroy {
-  @HostBinding('style.--board-layout-track-count')
+export class BoardLayoutComponent implements OnInit, AfterContentInit, OnDestroy {
   @Input()
   set tracks(val: number) {
     this._tracks = val;
     this.columnDefs = [...new Array(val).keys()];
-    setTimeout(() => this.reorder(), 0);
+    // setTimeout(() => this.reorder(), 0);
+    this._renderer.setStyle(
+      this._element.nativeElement,
+      '--board-layout-track-count',
+      val,
+      RendererStyleFlags2.DashCase
+    );
+    this.reorder();
   }
   get tracks(): number {
     return this._tracks;
   }
-
-  @HostBinding('style.--board-layout-container-height')
-  height: string;
 
   @ContentChildren(BoardCardDirective) set cards(value: QueryList<BoardCardDirective>) {
     this._cards = value;
@@ -42,25 +50,41 @@ export class BoardLayoutComponent implements AfterContentInit, OnDestroy {
 
   private _tracks: number;
   private _cards: QueryList<BoardCardDirective>;
-  private readonly _unsub: Subject<void>;
+  private _resizeObserver: ResizeObserver;
+  private readonly _unsub$: Subject<void>;
   columnDefs: any[] = [];
-  constructor() {
-    this._unsub = new Subject();
+  constructor(private readonly _element: ElementRef<HTMLElement>, private readonly _renderer: Renderer2) {
+    this._unsub$ = new Subject();
+  }
+
+  ngOnInit(): void {
+    this._resizeObserver = new ResizeObserver(() => this.onResized());
+    this._resizeObserver.observe(this._element.nativeElement);
   }
 
   ngAfterContentInit(): void {
     this.reorder();
-    this._cards.changes.pipe(takeUntil(this._unsub)).subscribe(() => {
+    this._cards.changes.pipe(takeUntil(this._unsub$)).subscribe(() => {
       this.reorder();
     });
   }
 
   ngOnDestroy(): void {
-    this._unsub.next();
-    this._unsub.complete();
+    this._unsub$.next();
+    this._unsub$.complete();
+
+    this._resizeObserver.unobserve(this._element.nativeElement);
+  }
+
+  private onResized(): void {
+    // TODO: remove double reorder when the max height is changed
+    // product of a rearrangement of the cards.
+    this.reorder();
   }
 
   private reorder(): void {
+    if (!this.cards) return;
+
     const trackHeight = [...this.columnDefs];
     let maxTrackHeight = 0;
     const cards = this.cards.toArray();
@@ -74,6 +98,54 @@ export class BoardLayoutComponent implements AfterContentInit, OnDestroy {
     }
 
     // set container height so that flex break works as expected
-    this.height = `${maxTrackHeight}px`;
+    this._renderer.setStyle(
+      this._element.nativeElement,
+      '--board-layout-container-height',
+      `${maxTrackHeight}px`,
+      RendererStyleFlags2.DashCase
+    );
   }
 }
+
+/**
+ 
+import { Directive, Output, EventEmitter, OnInit, OnDestroy, ElementRef } from '@angular/core';
+import ResizeObserver from 'resize-observer-polyfill';
+
+@Directive({
+  exportAs: 'resizeObserver',
+  selector: '[resizeObserver]'
+})
+export class ResizeObserverDirective implements OnInit, OnDestroy {
+  @Output('resizeObserver') resize = new EventEmitter<Partial<DOMRectReadOnly>>();
+
+  private _observer: ResizeObserver;
+  private _timer: any;
+
+  constructor(private readonly _el: ElementRef<HTMLElement>) {}
+
+  ngOnInit() {
+    this._observer = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        this.onResize(entry.contentRect);
+      }
+    });
+
+    this._observer.observe(this._el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this._observer.unobserve(this._el.nativeElement);
+  }
+
+  onResize(e: Partial<DOMRectReadOnly>) {
+    if (this._timer) {
+      clearTimeout(this._timer);
+      this._timer = undefined;
+    }
+
+    this._timer = setTimeout(() => this.resize.emit(e), 100);
+  }
+}
+
+ */
--- projects/ngx-board-layout/src/lib/ngx-board-layout.module.ts
+++ projects/ngx-board-layout/src/lib/ngx-board-layout.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { BoardCardDirective } from './board-card.directive';
 import { BoardLayoutComponent } from './board-layout.component';
 
